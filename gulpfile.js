e525b0ca38557f40c3bb1f66197655afce4808b6
AquiburRKhan <ark.aquib17@gmail.com>
2017-05-29 18:21:20 +0600

    change: changed gulpfile code

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     gutil = require('gulp-util');
 
-gulp.task('default', function() {
+gulp.task('scripts', function() {
     return gulp.src(['./promisePolyfill.js','./fetch.js','./rehiveSDK.js'])
         .pipe(concat('rehiveSdk.min.js'))
         .pipe(uglify())
         .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
         .pipe(gulp.dest('build'));
 });
+
+gulp.task('default', ['scripts']);
