1a157758302c8912fa93374c6b76a0b827c86efa
TarCode <tarcode33@gmail.com>
2018-03-26 16:07:54 +0200

    test next and previous filter server side

--- nodetest.js
+++ nodetest.js
@@ -3,16 +3,36 @@ const Rehive = require('./rehive')
 const r = new Rehive()
 
 r.auth.login({
-	user: 'stephan@rehive.com',
-	company: 'shape',
-	password: 'launch1234'
+	user: 'test1@rehive.com',
+	company: 'test_company_1',
+	password: 'test1234'
 }).then(function (user) {
 
+	// GET USER PROFILE
 	r.user.get().then(function (user) {
 		console.log("FROM USER METHOD", user);
+
 	}, function (err) {
 		console.log(err);
 	});
+
+	// GET TRANSACTIONS
+	r.transactions.get().then(function (res) {
+		console.log("TRANSACTIONS", res);
+
+		// GET NEXT TRANSACTIONS
+		r.transactions.getNext().then(function (res) {
+			console.log("NEXT TRANSACTIONS", res);
+
+		}, function (err) {
+			console.log(err);
+		});
+
+	}, function (err) {
+		console.log(err);
+	});
+
+
 
 }, function (err) {
 	console.log(err);
